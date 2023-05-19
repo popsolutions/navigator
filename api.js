9548158b56cd3c79259f7bbd65498cd73808d5f7
Marcos Méndez <mendez.foto@gmail.com>
2023-05-18 22:46:50 -0300

    Actualizando la funciona api.js fix

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const apiPort = process.env.API_PORT;
+const apiPort = process.env.API_PORT || 3000;
 
 async function updateConfig() {
     try {
@@ -36,34 +36,6 @@ app.post("/blacklist/remove", checkAdmin, async (req, res) => {
     } catch (e) {
         res.status(400).send(e);
     }
-});
-
-function checkAdmin(req, res, next) {
-    const token = req.body.token;
-    const adminToken = bot.config.adminToken;
-
-    if (adminToken === token) {
-        next(); // pass the request to the next middleware
-    } else {
-        res.status(403).send("Error: Access denied"); // send a 403 Forbidden error response
-    }
-}
-
-app.post("/blacklist/add", checkAdmin, (req, res) => {
-    try {
-        bot.addGroupBlackList(req.body.id);
-        updateConfig();
-        res.sendStatus(200);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-
-});
-
-app.post("/blacklist/remove", checkAdmin, (req, res) => {
-    bot.removeGroupBlackList(req.body.id);
-    updateConfig();
-    res.sendStatus(200);
 });
 
 app.listen(apiPort, () => {
